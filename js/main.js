392c5358f2f2dccba8ac7dde4a260b58d8d464f7
Tyler Nielsen <tynielsen@gmail.com>
2013-12-09 21:33:36 -0700

    moving js out of markup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,74 @@
+(function($) {
+    // This is the connector function.
+    // It connects one item from the navigation carousel to one item from the
+    // stage carousel.
+    // The default behaviour is, to connect items with the same index from both
+    // carousels. This might _not_ work with circular carousels!
+    var connector = function(itemNavigation, carouselStage) {
+        return carouselStage.jcarousel('items').eq(itemNavigation.index());
+    };
+
+    $(function() {
+        // Setup the carousels. Adjust the options for both carousels here.
+        var carouselStage      = $('.carousel-stage').jcarousel();
+        var carouselNavigation = $('.carousel-navigation').jcarousel();
+
+        // We loop through the items of the navigation carousel and set it up
+        // as a control for an item from the stage carousel.
+        carouselNavigation.jcarousel('items').each(function() {
+            var item = $(this);
+
+            // This is where we actually connect to items.
+            var target = connector(item, carouselStage);
+
+            item
+                .on('jcarouselcontrol:active', function() {
+                    carouselNavigation.jcarousel('scrollIntoView', this);
+                    $(item.prevAll('li')
+                    .get()
+                    .reverse())
+                    .appendTo('.carousel-navigation ul');
+
+                    //subtle slide back up to featured images
+                    $('body').animate({
+                      scrollTop: 0
+                    }, 'slow');
+                })
+                .on('jcarouselcontrol:inactive', function() {
+                    item.removeClass('active');
+                })
+                .jcarouselControl({
+                    target: target,
+                    carousel: carouselStage
+                });
+        });
+
+        // Setup controls for the stage carousel
+        $('.prev-stage')
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.next-stage')
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+    });
+})(jQuery);
+
+
 $(window).load(function() {
   
 
@@ -107,79 +178,40 @@ $(window).load(function() {
     });
   }
 
+
+  $('.carousel-stage ul li a').fancybox({'padding': 0});
+
+      $('.carousel-stage').on('jcarousel:scrollend', function(event, carousel) {
+
+        var first = $('.carousel-stage').jcarousel('visible');
+        
+        $('.carousel-stage li').removeClass('visible'); 
+        first.addClass('visible');
+      });
+
+      $('.carousel-stage')
+        .jcarousel({
+          wrap: 'circular'
+          //center: true
+        })
+        .jcarousel('scroll', 1);
+
+      $('.prev')
+      .jcarouselControl({
+        target: '-=1'
+      });
+      
+      $('.next')
+      .jcarouselControl({
+        target: '+=1'
+      });
+    
+      //Zoom in effect for looks page images
+      //$('.generic-popup').zoom({
+        //url: 'img/featured-img-3-2.jpg',
+        //on: 'click'
+      //});
+
 });
-
-(function($) {
-    // This is the connector function.
-    // It connects one item from the navigation carousel to one item from the
-    // stage carousel.
-    // The default behaviour is, to connect items with the same index from both
-    // carousels. This might _not_ work with circular carousels!
-    var connector = function(itemNavigation, carouselStage) {
-        return carouselStage.jcarousel('items').eq(itemNavigation.index());
-    };
-
-    $(function() {
-        // Setup the carousels. Adjust the options for both carousels here.
-        var carouselStage      = $('.carousel-stage').jcarousel();
-        var carouselNavigation = $('.carousel-navigation').jcarousel();
-
-        // We loop through the items of the navigation carousel and set it up
-        // as a control for an item from the stage carousel.
-        carouselNavigation.jcarousel('items').each(function() {
-            var item = $(this);
-
-            // This is where we actually connect to items.
-            var target = connector(item, carouselStage);
-
-            item
-                .on('jcarouselcontrol:active', function() {
-                    carouselNavigation.jcarousel('scrollIntoView', this);
-                    $(item.prevAll('li')
-                    .get()
-                    .reverse())
-                    .appendTo('.carousel-navigation ul');
-
-                    //subtle slide back up to featured images
-                    $('body').animate({
-                      scrollTop: 0
-                    }, 'slow');
-                })
-                .on('jcarouselcontrol:inactive', function() {
-                    item.removeClass('active');
-                })
-                .jcarouselControl({
-                    target: target,
-                    carousel: carouselStage
-                });
-        });
-
-        // Setup controls for the stage carousel
-        $('.prev-stage')
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('.next-stage')
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .jcarouselControl({
-                target: '+=1'
-            });
-    });
-})(jQuery);
-
-
-
 
 
